1dc70e3c82eb1b7d6e1fcab050060d03b5f5a248
mona lisa <lizfriedman5@gmail.com>
2023-11-07 17:46:39 -0600

    footer format change

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -8,7 +8,9 @@ const Footer = () => {
                     <div className='footer-text'>about</div>
                     <div className='footer-text'>developers</div>
                     <div className='footer-text'>careers</div>
-                    <div className='footer-text'>&copy; 2023 scripts. All rights reserved.</div>
+            </div>
+            <div>
+            <div className='footer-copyright'>&copy; 2023 scripts. All rights reserved.</div>
 
             </div>
 

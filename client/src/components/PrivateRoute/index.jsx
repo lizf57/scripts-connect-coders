67be23e68eea639332674ea9190dbdd5d8759a99
Marketing Director <nicolestark1103@gmail.com>
2023-11-08 13:22:27 -0600

    saved

--- client/src/components/PrivateRoute/index.jsx
+++ client/src/components/PrivateRoute/index.jsx
@@ -1,12 +1,7 @@
 import { Navigate } from "react-router-dom"
-// import { useLocalState } from "../../utils/useLocalStorage"
 
-import Auth from '../../utils/auth'
 
 const PrivateRoute = ({ children }) => {
-
-    // const [id_token, setId] = useLocalState('', 'id_token')
-    // console.error()
 
     const id = localStorage.getItem('id_token')
 

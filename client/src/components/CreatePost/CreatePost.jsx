1b9b9e6496923b4c61d49415c78ba01fa3db0983
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-11-07 20:17:02 -0600

    adding character counting to post

--- client/src/components/CreatePost/CreatePost.jsx
+++ client/src/components/CreatePost/CreatePost.jsx
@@ -1,8 +1,41 @@
-import { Button, useColorModeValue } from "@chakra-ui/react"
+import { Button, useColorModeValue, useDisclosure, Text, FormControl, Textarea } from "@chakra-ui/react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
+import { useState } from "react"
 
+const charLimit = 255
+let charMessage = ""
 const CreatePost = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [postText,setPostText] = useState('')
+    const [remainingChar, setRemainingChar] = useState(charLimit)
+    const handleTextChange = (e) => {
+      const inputEntry = e.target.value
+
+      if(inputEntry.length > charLimit) {
+        setPostText(inputEntry.slice(0,charLimit))
+        setRemainingChar(0)
+        charMessage = "Character limit reached"
+      } else {
+        setPostText(inputEntry)
+        setRemainingChar(charLimit - inputEntry.length)
+        charMessage = remainingChar + " characters remaining"
+      }
+    }
+    const handleCreatePost = async () => {
+      console.log("not working just yet")
+    }
+    // TODO Add Images upload when we figure out cloudinary?
+    // TODO: fix bug with character limit
   return (
     <>
     <Button
@@ -11,9 +44,39 @@ const CreatePost = () => {
         right={10}
         leftIcon={<FontAwesomeIcon icon={faPlus} />}
         bg={useColorModeValue("gray.900","gray.200")}
+        onClick={onOpen}
     >
         Post
     </Button>
+
+    <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={4}>
+
+          <FormControl>
+            <Textarea 
+              placeholder="Enter what you want"
+              onChange={handleTextChange}
+              value={postText}              
+            />
+            
+            <Text fontSize={"xs"} textAlign={"right"} fontWeight={"bold"}>
+              {charMessage}
+            </Text>
+          </FormControl>
+
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={handleCreatePost}>
+              Post
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
 
     </>
   )

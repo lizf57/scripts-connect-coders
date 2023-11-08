be632fff725da5dec5464cfe43e5459a75341416
Marketing Director <nicolestark1103@gmail.com>
2023-11-08 11:55:59 -0600

    econnrefused error for login and signup, might be an issue with my computer

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import Typewriter from "typewriter-effect"
+import { useState } from "react"
+import { Flex, Input, Link, Text, Button, Stack, FormControl, FormLabel, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
-import Auth from '../utils/auth';
+import Auth from '../utils/auth'
+
+
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -22,12 +25,11 @@ const Login = (props) => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    console.log(formState);
     try {
       const { data } = await login({
         variables: { ...formState },
       });
-
+      
       Auth.login(data.login.token);
     } catch (e) {
       console.error(e);
@@ -39,56 +41,102 @@ const Login = (props) => {
       password: '',
     });
   };
+  
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-info"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
+    <>
+      <Flex>
+        <Flex 
+          bg={'darkBlue'} 
+          w={'50vw'} 
+          h={'100vh'} 
+          justifyContent={'start'} 
+          alignItems={'center'}
+          p={'30px'}
+          fontFamily={'sans-serif'}
+          fontWeight={'800'}
+          fontSize={'40px'}
+          color={'lightPurple'}
+        >
+              <Typewriter
+                  options={{
+                    loop: true,
+                  }}
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-};
+                  onInit={(typewriter) => {
+                      typewriter
+                          .typeString("connecting coders")
+                          .pauseFor(1000)
+                          .deleteAll()
+                          .typeString("one script at a time.")
+                          .pauseFor(1000)
+                          .start();
+                  }}
+              />
+          </Flex>
+          
+          <Flex 
+            justifyContent={'end'}
+            bg={'darkBlue'}
+            w={'50vw'}
+            h={'100vh'}
+            alignItems={'center'}
+            p={'30px'}
+          >
+            <Flex flexDir={'column'} w={'100vw'}>
 
-export default Login;
+              <Text mb={10} fontSize={'30px'} fontWeight={'700'} color={'lightPurple'}>
+                Login
+              </Text>
+
+              {data ? (
+                <p>Success!</p>
+              ) : (
+                <form onSubmit={handleFormSubmit}>
+
+                <FormControl isRequired mb={7}>
+                  <FormLabel>Email</FormLabel>
+                  <Input placeholder='email' borderColor={'neonBlue'} focusBorderColor={'lightPurple'} name='email' type='email' value={formState.email} onChange={handleChange} />
+                </FormControl>
+
+                <FormControl isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <Input placeholder='password' borderColor={'neonBlue'} focusBorderColor={'lightPurple'} name='password' type='password' value={formState.password} onChange={handleChange} />
+                </FormControl>
+
+                <Stack direction='row' spacing={4} mt={7}>
+                  <Button bg={'neonBlue'} variant='solid' type='submit'>
+                    Login
+                  </Button>
+                  <Link href='/signup'>
+                    <Button colorScheme='purple' borderColor={'lightPurple'} variant='outline'>
+                      Sign Up
+                    </Button>
+                  </Link>
+                  
+                </Stack>
+
+                </form>
+
+              )}
+
+              {error && (
+                <div>{console.log(error)}</div>
+              )}
+
+
+             
+
+            </Flex>
+
+           
+            
+          </Flex>
+
+        </Flex>
+
+    </>
+  )
+}
+
+export default Login

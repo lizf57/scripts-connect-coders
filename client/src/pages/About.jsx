e95d87ff5dbae47483963f88abe041e3a05ac2c9
mona lisa <lizfriedman5@gmail.com>
2023-11-07 21:25:30 -0600

    footer links: about, careers, developers

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,15 +1,61 @@
-import { Container } from '@chakra-ui/react'
+import { Stack, Text, Heading, Card, CardBody, Container, } from '@chakra-ui/react'
+
+const lists = [
+    {
+        title: "our purpose",
+        description: "scripts serves as a vibrant hub for the coding community. Uniting individuals with shared passions for web development. A marketing tool for business. An avenue for philanthropic contributions. A way to get connected.",
+    },
+    {
+        title: "connect with coders",
+        description: "create an account with scripts and start networking with web developers.",
+    },
+    {
+        // TODO: STRIPE PAYMENT - SET UP LINK
+        title: "give back",
+        description: "link for stripe",
+    },
+    {
+        // TODO: Set up link to career page
+        title: "become part of the team",
+        description: "link to career page",
+    }
+]
 
 const About = () => {
+
     return (
-        <>
+        <> 
+      
         <Container maxW='700px' mb={40}>
-                
-                
-            About
+            
+        {lists.map(list => {
+            return (
+
+            <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='filled'
+            >
+
+            <Stack>
+
+                <CardBody>
+
+                <Heading size='xl' style={{color: '#cb6ce6'}} >{list.title}</Heading>
+
+                <Text size='md' style={{color: '#5371FF'}}>{list.description}</Text>
+
+                </CardBody>
+
+            </Stack>
+
+        </Card>
+            )
+        })}
 
         </Container>
-        </>
+      
+       </>
     )
 }
 
--- client/src/pages/Developers.jsx
+++ client/src/pages/Developers.jsx
@@ -1,15 +1,55 @@
-import { Container } from '@chakra-ui/react'
+import { Stack, Text, Heading, Card, CardBody, Container, } from '@chakra-ui/react'
+
+const developers = [
+    {
+        name: "Tor Jansson",
+        github: "",
+    },
+    {
+        name: "Nicole Stark",
+        github: "",
+    },
+    {
+        name: "Liz Friedman",
+        github: "",
+    },
+    
+]
 
 const Developers = () => {
     return (
-        <>
+        <> 
+      
         <Container maxW='700px' mb={40}>
-                
-                
-            Developers
+            
+        {developers.map(developer => {
+            return (
+
+            <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='filled'
+            >
+
+            <Stack>
+
+                <CardBody>
+
+                <Heading size='xl' style={{color: '#cb6ce6'}} >{developer.name}</Heading>
+
+                <Text size='md' style={{color: '#5371FF'}}>{developer.github}</Text>
+
+                </CardBody>
+
+            </Stack>
+
+        </Card>
+            )
+        })}
 
         </Container>
-        </>
+      
+       </>
     )
 }
 
